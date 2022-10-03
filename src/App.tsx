3961dab7018263e33c82c4f7731882284c212f8f
João Francisco <joaofrancisco.guarda@gmail.com>
2022-10-03 20:28:45 -0300

    fix: not used references

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Calculator from "./pages/calculator";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { GlobalStyle } from "./styles/global";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
